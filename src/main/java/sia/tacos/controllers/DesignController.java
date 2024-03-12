1cf54e0f56c6d7f9a9bcbea78ab9d701a3ec8368
Ilya <ilya.chumak1990@gmail.com>
2024-03-13 00:39:16 +0300

    feat/add validation annotations in design and order controllers

--- src/main/java/sia/tacos/controllers/DesignController.java
+++ src/main/java/sia/tacos/controllers/DesignController.java
@@ -6,12 +6,14 @@ import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import sia.tacos.entities.Ingredient;
 import sia.tacos.entities.Ingredient.Type;
@@ -61,7 +63,10 @@ public class DesignController {
     }
 
     @PostMapping
-    public String processTaco(Taco taco, @ModelAttribute TacoOrder tacoOrder){
+    public String processTaco(@Valid Taco taco, Errors errors, @ModelAttribute TacoOrder tacoOrder){
+        if(errors.hasErrors()){
+            return "design";
+        }
         tacoOrder.addTaco(taco);
         log.info("Processing taco: {}", taco);
         return "redirect:/orders/current";
