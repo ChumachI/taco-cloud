692406a8781fc093429d131bf31a0f4b9aeb3b04
Ilya <ilya.chumak1990@gmail.com>
2024-03-12 23:27:19 +0300

    feat/add PostMapping in OrderController

--- src/main/java/sia/tacos/controllers/OrderController.java
+++ src/main/java/sia/tacos/controllers/OrderController.java
@@ -2,10 +2,14 @@ package sia.tacos.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 import lombok.extern.slf4j.Slf4j;
+import sia.tacos.entities.TacoOrder;
+
 
 
 @Slf4j
@@ -13,10 +17,17 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/orders")
 @SessionAttributes("tacoOrder")
 public class OrderController {
-    
+
     @GetMapping("/current")
     public String orderForm() {
         return "orderForm";
+    }
+    
+    @PostMapping
+    public String processOrder(TacoOrder order, SessionStatus sessionStatus) {
+        log.info("Order submitted: {}", order);
+        sessionStatus.setComplete();
+        return "redirect:/";
     }
     
 }
